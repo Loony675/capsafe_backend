28020764c61f04880b071503a6ba333037251758
Loussot <theo.loussot@gmail.com>
2022-11-03 17:01:06 +0100

    parce que j'ai pas le choix putain

--- models/message.js
+++ models/message.js
@@ -1,21 +1,9 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-=======
-
->>>>>>> 89a4a1fb38cb581b56cf0cc78bbaf6f1974ebd2b
 const messageSchema = mongoose.Schema([
   {
     token1: String,
     token2: String,
     chanel: String,
-<<<<<<< HEAD
-    messageContent: [{
-      message: String,
-      username: String,
-      timestamp: String,
-      sended: Boolean
-     }],
-=======
     messageContent: [
       { message: String,
         name: String,
@@ -23,7 +11,6 @@ const messageSchema = mongoose.Schema([
         received: Boolean
       },
     ],
->>>>>>> 89a4a1fb38cb581b56cf0cc78bbaf6f1974ebd2b
   },
 ]);
 
