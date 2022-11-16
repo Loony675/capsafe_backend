cafed1c0b44d0986b2cf5428d9974bf69eb99443
Loussot <theo.loussot@gmail.com>
2022-11-16 13:07:49 +0100

    commentaires

--- models/message.js
+++ models/message.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const messageSchema = mongoose.Schema([
   {
-    token1: String,
-    token2: String,
-    chanel: String,
-    messageContent: [{
+    token1: String, // Token de l'utilisateur qui est connecté
+    token2: String, // Token de la personne que l'on souhaite contacté par chat
+    chanel: String, // Chanel unique aux 2 utilisateurs
+    messageContent: [{ // Contenu d'un message
       message: String,
       username: String,
       timestamp: String,
